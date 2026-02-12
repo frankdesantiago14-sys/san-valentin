f9065c8e1a19645867234a130642b2afacbb84ef
frankdesantiago14-sys <frankdesantiago14@gmail.com>
2026-02-12 00:24:49 -0600

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,83 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="romantic-page">
+
+      <div className="content">
+
+        {/* CANCIÓN */}
+        <section className="section">
+          <img src="public/caratula.jpg" alt="Caratula" className="cover" />
+
+          <h2>Gracias a Ti</h2>
+   
+
+          <audio controls>
+            <source src="/cancion.mp3" type="audio/mpeg" />
+          </audio>
+
+          <p className="scroll-text">Desliza ↓</p>
+        </section>
+
+        {/* FOTOS */}
+        <section className="section">
+          <h2>Nuestros Momentos</h2>
+
+          <div className="gallery">
+            <img src="public/foto1.jpg" alt="" />
+            <img src="public/foto2.jpg" alt="" />
+            <img src="public/foto3.jpg" alt="" />
+            <img src="public/foto4.jpg" alt="" />
+
+          </div>
+        </section>
+
+        {/* PREGUNTA */}
+        <section className="section question">
+          <h2>Quiero decirte algo…</h2>
+
+          <p>
+            No quiero repetir el pasado.  
+            Quiero hacerlo mejor.  
+            <br /><br />
+            No te presiono…  
+            solo quiero ser honesto contigo.  
+            <br /><br />
+            ¿Te gustaría ser mi San Valentín  
+            y ver qué pasa si lo intentamos bien esta vez? ❤️
+          </p>
+
+          <div className="buttons">
+<button
+  className="yes"
+  onClick={() =>
+    window.open(
+      "https://wa.me/5214413900424?text=Sí,%20quiero%20ser%20tu%20San%20Valentín%20❤️",
+      "_blank"
+    )
+  }
+>
+  Sí, intentémoslo ❤️
+</button>
+<button
+  className="yes"
+  onClick={() =>
+    window.open(
+      "https://wa.me/5214413900424?text=Sí,%20quiero%20ser%20tu%20San%20Valentín%20❤️",
+      "_blank"
+    )
+  }
+>
+  Hablemos ❤️
+</button>
+          </div>
+        </section>
+
+      </div>
+    </div>
+  );
+}
+
+export default App;
+
